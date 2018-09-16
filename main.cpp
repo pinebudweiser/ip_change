dfc35789fbbbd9d3a7ed704022e5082d4fbc2836
pinebudweiser <pinebudweiser@gmail.com>
2018-09-16 02:40:54 -0400

    S0rry

--- main.cpp
+++ main.cpp
@@ -4,10 +4,7 @@
 #include <libnetfilter_queue/libnetfilter_queue.h>
 #include "my-tools.h"
 
-
-/* */
 #define MAX_PACKET      0xFFFF
-#define PROTOCOL_TCP	0x6
 #define VERSION_IPV4	0x4
 #define HTTP_PORT		80
 
@@ -23,11 +20,14 @@ typedef struct nfqnl_msg_packet_hdr nfqnl_msg_packet_hdr;
 typedef struct libnet_ipv4_hdr IP;
 typedef struct libnet_tcp_hdr TCP;
 
+/* static */
+static char* before_ip;
+static char* after_ip;
+
 using namespace std;
 
 int queue_processor(nfq_q_handle *CrtHandle, nfgenmsg *nfmsg,
-                     nfq_data *packet_handler, void *data)
-{
+                     nfq_data *packet_handler, void *data){
     uint32_t pktLen; // IP+TCP+HTTP, HTTP
     int id;
     uint8_t* packet;
@@ -35,47 +35,52 @@ int queue_processor(nfq_q_handle *CrtHandle, nfgenmsg *nfmsg,
     TCP* tcpHeader;
     nfqnl_msg_packet_hdr *packetHeader;
     MyTool binTool;
-    uint16_t result;
     PSEUDO_HEADER pse;
     uint8_t* my_packet = 0;
 
     packetHeader = nfq_get_msg_packet_hdr(packet_handler);
-    if (packetHeader) {
+    if (packetHeader)
         id = ntohl(packetHeader->packet_id);
-    }
     pktLen = nfq_get_payload(packet_handler, &packet); // get packet - starting IP
     ipHeader = (IP*)(packet);
 
-    if(ipHeader->ip_v == VERSION_IPV4)
-    {
-        //memcpy(&(ipHeader->ip_dst), "\x7D\x8E\x43\x8E", 4);
+    if(ipHeader->ip_v == VERSION_IPV4){
         ipHeader->ip_sum = 0;
+        printf("Dest ip = %s\n", inet_ntoa(ipHeader->ip_dst));
+        printf("before_ip = %s\n", before_ip);
+        if(inet_addr(inet_ntoa(ipHeader->ip_dst)) == inet_addr(before_ip))
+        {
+            inet_aton(after_ip, &(ipHeader->ip_dst));
+            printf("Change DestIP : %s\n", inet_ntoa(ipHeader->ip_dst));
+        }
         binTool.init((uint8_t*)ipHeader,sizeof(IP)); // arg1 = index of data, arg2 = HTTP data size.
-        ipHeader->ip_sum = ntohs(binTool.GetCheckSum());
+        ipHeader->ip_sum = htons(binTool.GetCheckSum());
         switch(ipHeader->ip_p)
         {
             case IPPROTO_ICMP:
                 break;
             case IPPROTO_TCP:
                 tcpHeader = (TCP*)(packet+(ipHeader->ip_hl << 2));
-                //if(ntohs(tcpHeader->th_dport) == HTTP_PORT) // response data
-                //{
-                tcpHeader->th_sum = 0;
+                tcpHeader->th_sum = 0; // checksum init
+                /* set pseudo_header */
                 memcpy(pse.src_addr, &(ipHeader->ip_src), 4);
                 memcpy(pse.dst_addr, &(ipHeader->ip_dst), 4);
                 pse.protocol = ipHeader->ip_p;
-                pse.tcp_length = htons(tcpHeader->th_off << 2);
-                binTool.init((uint8_t*)tcpHeader, pktLen-(ipHeader->ip_hl << 2));
+                pse.tcp_length = htons(pktLen - (ipHeader->ip_hl << 2)); // tcp_header + data
+                /* end */
+                my_packet = (uint8_t*)malloc(pktLen - (ipHeader->ip_hl << 2) + sizeof(PSEUDO_HEADER));
+                memcpy(my_packet, &pse, sizeof(PSEUDO_HEADER));
+                memcpy(my_packet + sizeof(PSEUDO_HEADER), tcpHeader, pktLen - (ipHeader->ip_hl<<2));
+                binTool.init(my_packet, pktLen - (ipHeader->ip_hl << 2) + sizeof(PSEUDO_HEADER));
                 tcpHeader->th_sum = htons(binTool.GetCheckSum());
-                return nfq_set_verdict(CrtHandle, id, NF_ACCEPT, pktLen, packet);
-                //}
+                free(my_packet);
                 break;
             case IPPROTO_UDP:
                 break;
         }
 
     }
-    //return nfq_set_verdict(CrtHandle, id, NF_ACCEPT, pktLen, packet);
+    return nfq_set_verdict(CrtHandle, id, NF_ACCEPT, pktLen, packet);
 }
 
 int main(int argc, char** argv)
@@ -87,8 +92,12 @@ int main(int argc, char** argv)
     uint8_t* packet;
     char buf[4096];
 
-    if(argc != 3)
+    if(argc != 3){
         printf("Usage: ip_change <before_ip><change_ip>\n");
+        return 0;
+    }
+    before_ip = argv[1];
+    after_ip = argv[2];
 
 
     nfqOpenHandle = nfq_open();
